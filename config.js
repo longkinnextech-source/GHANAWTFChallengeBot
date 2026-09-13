6741d407d2b2426b348b777eabd8ad8f2b9bfbc4
longkinnextech-source <longkinnex.tech@gmail.com>
2026-09-13 23:23:08 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://vagbugmkidvhfcfauxoa.supabase.co";
+‎const SUPABASE_ANON_KEY = "sb_publishable_KqJ9PHlymqWiINVqCoaA9Q_all_M7fA";
+‎
+‎const BIRTHDAY_START = "2026-09-14T06:00:00Z";
+‎const BIRTHDAY_END = "2026-09-17T06:00:00Z";
+‎
+‎const BIRTHDAY_CODE = "longthingbirthday";
