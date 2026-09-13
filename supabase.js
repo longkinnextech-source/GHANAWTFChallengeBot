98f5e850b6092133ac28571469600b580b9c1766
longkinnextech-source <longkinnex.tech@gmail.com>
2026-09-13 23:58:51 +0000

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+‎const SUPABASE_URL = "https://vagbugmkidvhfcfauxoa.supabase.co";
+‎const SUPABASE_ANON_KEY = "sb_publishable_KqJ9PHlymqWiINVqCoaA9Q_all_M7fA";
+‎
+‎const supabase = window.supabase.createClient(
+‎  SUPABASE_URL,
+‎  SUPABASE_ANON_KEY
+‎);
